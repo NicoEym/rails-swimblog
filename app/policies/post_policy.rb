d21dbf6bc8b20d7239081a7b19d45618b3443242
nicolas <nicolas.eymenier@gmail.com>
2020-10-31 17:52:56 -0300

    added policies to post policy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,34 @@
 class PostPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      # a user can see all the beaches
+      @scope.all
     end
+  end
+
+  def create?
+    # only the admin can create a new beach
+    user_is_admin?
+  end
+
+  def show?
+    # a user can see any beach
+    true
+  end
+
+  def update?
+    # only the admin can update a new beach
+    user_is_admin?
+  end
+
+  def destroy?
+    # only the admin can destroy a new beach
+    user_is_admin?
+  end
+
+  private
+
+  def user_is_admin?
+    @user.admin?
   end
 end
