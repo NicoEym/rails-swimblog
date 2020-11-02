02040c311dbf43a35ef28df82b20efc996dfa4db
nicolas <nicolas.eymenier@gmail.com>
2020-11-02 11:01:51 -0300

    installed places from algolia

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ console.log('Hello World from Webpacker')
 // External imports
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -33,8 +34,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 
-
-
   initMapbox();
+  initAutocomplete();
 
 });
