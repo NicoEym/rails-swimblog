6e947894f1ca286d5761411b6380b160bf84aa7a
nicolas <nicolas.eymenier@gmail.com>
2020-12-01 22:36:45 -0300

    add title to cards and hide the banner in JS

--- app/javascript/plugins/init_scroll.js
+++ app/javascript/plugins/init_scroll.js
@@ -3,15 +3,17 @@ const initScroll = () => {
   const map_container = document.getElementById('map-page');
   const card_container = document.getElementById('card-container');
   const footer = document.getElementById('footer');
+  const banner = document.querySelector('.banner');
   if (button) {
     button.addEventListener('click', (e) => {
         if (map_container.classList.contains("hidden-style")) {
           map_container.classList.remove("hidden-style");
           card_container.classList.remove("hidden-style");
           footer.classList.remove("hidden-style");
+
         }
       map_container.scrollIntoView({behavior: "smooth", block: "start"});;
-
+      banner.classList.add("hidden-style");
 
     });
 
